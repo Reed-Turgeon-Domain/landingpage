e5f22396590055556fa1e64b213764157e16e8c1
MrT <turgeon.dev@gmail.com>
2025-01-13 20:13:31 -0700

    google analytics > attempt 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import partytown from '@astrojs/partytown' // optimizing third-party scripts
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind(),
+    partytown({
+      config: {
+        // Forward specific functions from main thread to web worker
+        // In this case, forwarding dataLayer.push for Google Tag Manager
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
+  // Define the production site URL
   site: 'https://reedturgeon.com',
+  // Set the base path for the site (/ means root)
   base: '/',
 })
